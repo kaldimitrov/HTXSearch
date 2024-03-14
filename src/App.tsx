80853487f803da87e9a99e5cfc5a5c63895876dd
mmarto113 <karabonevmartin@gmail.com>
2024-03-14 12:00:55 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          HTXSearch
+        </p>
+        <SearchBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+
+export default function SearchBar (){
+
+    return (
+        <>
+            <Paper
+                component="form"
+                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+            >
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search"
+                inputProps={{ 'aria-label': 'search' }}
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon />
+            </IconButton>
+            </Paper>
+        </>
+    )
+}
