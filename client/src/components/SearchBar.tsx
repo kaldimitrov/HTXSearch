2208b0a57417f8f52063d71fbae6a5a687c77fcb
kdimitrov05 <k.dimitrov@zettahost.com>
2024-03-14 15:37:31 +0200

    Updated page layout and design

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -4,16 +4,26 @@ import SearchIcon from "@mui/icons-material/Search";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 
-export default function SearchBar() {
+export default function SearchBar(props: { updateValue: any; input: string }) {
   return (
     <>
       <Paper
         component="form"
-        sx={{ p: "4px 4px", display: "flex", alignItems: "center" , borderRadius: 3, boxShadow: 3}}
+        sx={{
+          p: "4px 4px",
+          display: "flex",
+          alignItems: "center",
+          borderRadius: 3,
+          boxShadow: 3,
+        }}
       >
         <InputBase
           sx={{ ml: 1, flex: 1, padding: 1 }}
           placeholder="Search"
+          value={props.input}
+          onInput={(e: any) => {
+            props.updateValue(e.target.data);
+          }}
           inputProps={{ "aria-label": "search" }}
         />
         <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
--- client/src/components/SuggestionCard.tsx
+++ client/src/components/SuggestionCard.tsx
@@ -1,22 +1,28 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { start } from 'repl';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
-export default function SuggestionCard(){
-    return (
-        <>
-            <Card sx={{ width: 0.98 / 3, cursor: 'pointer' }} onClick = {() => {alert('yes')}}>
-                <CardContent>
-                    <Typography sx={{ fontSize: 15, textAlign: 'start'}} color="text.secondary" gutterBottom>
-                        Kakuv e Kamen?
-                    </Typography>
-                </CardContent>
-            </Card> 
-        </>
-    )
+export default function SuggestionCard(props: {
+  text: string;
+  updateValue: Function;
+}) {
+  return (
+    <>
+      <Card
+        sx={{ width: 0.98 / 3, height: 1 / 4, cursor: "pointer" }}
+        onClick={() => props.updateValue(props.text)}
+      >
+        <CardContent>
+          <Typography
+            sx={{ fontSize: 15, textAlign: "start" }}
+            color="text.secondary"
+            gutterBottom
+          >
+            {props.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
