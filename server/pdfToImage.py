b89b581dc026652be1d0c1fc954fa2d9f48e659b
Kamen <kamen2109@gmail.com>
2024-03-14 16:28:07 +0200

    pdf2image to pdfToImage

---
+++ server/pdfToImage.py
@@ -0,0 +1,7 @@
+from pdf2image import convert_from_path
+
+def pdfToImage(input):
+    images = convert_from_path(input)
+ 
+    for i in range(len(images)):
+        images[i].save('page'+ str(i) +'.jpg', 'JPEG')
